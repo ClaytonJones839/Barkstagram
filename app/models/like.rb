13dcec6ef50ad6afe0a0ab7b5adcd59ad0d0823c
Clayton <claytonajones839@gmail.com>
2019-08-05 11:38:22 -0400

    Add validations to likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
+
+  validates :user_id, :post_id, presence: true
+  validates :user_id, uniqueness: { scope: :post_id }
   belongs_to :user
   belongs_to :post
+
+
 end
