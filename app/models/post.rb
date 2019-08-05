0d9c50c45b83c58a2dd83d2e26678b8a50abb715
Clayton <claytonajones839@gmail.com>
2019-08-05 12:20:30 -0400

    Add likes and likers association to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,11 @@ class Post < ApplicationRecord
     
     belongs_to :user
     has_one_attached :photo
+    has_many :likes,
+        dependent: :destroy
+    has_many :likers,
+        through: :likes,
+        source: : user_id
+
 
 end
