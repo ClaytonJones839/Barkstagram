6b2aa791096ebdcf80f4d73580e655ad089ad08d
Clayton <claytonajones839@gmail.com>
2019-08-05 12:05:17 -0400

    Add actions#likes_actions

---
+++ frontend/actions/likes_actions.js
@@ -0,0 +1,29 @@
+import * as LikeAPIUtil from '../util/like_api_util';
+
+export const RECEIVE_LIKE = 'RECEIVE_LIKE';
+export const REMOVE_LIKE = 'REMOVE_LIKE';
+
+const receiveLike = like => ({
+    type: RECEIVE_LIKE,
+    like
+});
+
+const removeLike = like => ({
+    type: REMOVE_LIKE,
+    like
+});
+
+export const createLike = like => dispatch => {
+    return (
+        LikeAPIUtil.createLike(like)
+            .then(like_id => (dispatch(receiveLike(like_id))))
+    )
+};
+
+export const deleteLike = like_id => dispatch => {
+    return (
+        LikeAPIUtil.deleteLike(like_id)
+            .then(like => (dispatch(removeLike(like))))
+    )
+};
+
