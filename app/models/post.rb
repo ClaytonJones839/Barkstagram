62862552192099c40ff48af4bf286541b4dacef4
Clayton <claytonajones839@gmail.com>
2019-08-06 11:33:21 -0400

    Add associations and validations to comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,16 @@
 class Post < ApplicationRecord
-    
+    validates :body, presence: true
+
     belongs_to :user
     has_one_attached :photo
+
     has_many :likes,
         dependent: :destroy
+
     has_many :likers,
         through: :likes,
         source: :user
 
+    has_many :comments
 
 end
