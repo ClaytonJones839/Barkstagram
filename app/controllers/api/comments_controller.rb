870629a508f546cabc9669f04bc3b9e89b124518
Clayton <claytonajones839@gmail.com>
2019-08-06 11:39:49 -0400

    Add comment controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,31 @@
+class Api::CommentsController < ApplicationController
+
+    def show
+    end
+
+    def create 
+        @comment = Comment.new(comment_params)
+        @comment.user_id = current_user.id 
+
+        if @comment.save
+            render :show
+        else
+            render json: @comment.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @comment = Comment.find_by(params[:id])
+        if @comment && @comment.user_id == current_user.id
+            @comment.destroy
+            render :show
+        else
+            render json: ["User is only able to remove their own comments"], status: 422
+        end
+    end
+
+    private
+        def like_params
+            params.require(:comment).permit(:post_id, :body)
+        end
+end
