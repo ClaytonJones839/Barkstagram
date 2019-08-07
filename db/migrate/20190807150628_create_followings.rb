774962f072c3af96ba083955630e3eb036b6a827
Clayton <claytonajones839@gmail.com>
2019-08-07 11:10:51 -0400

    Add follow table migration

---
+++ db/migrate/20190807150628_create_followings.rb
@@ -0,0 +1,14 @@
+class CreateFollowings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :followings do |t|
+      t.references :user, foreign_key: true
+      t.integer :followed_user_id, null: false
+
+      t.timestamps
+    end
+      add_index :follows, :followed_user_id
+      add_index :follows, [:user_id, :followed_user_id], unique: true
+
+
+  end
+end
