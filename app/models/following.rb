6862f3bea7d0705c327c771a52256fb9de69a450
Clayton <claytonajones839@gmail.com>
2019-08-07 11:20:17 -0400

    Add following model validations and associations

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,9 @@
 class Following < ApplicationRecord
+    validates :user_id, uniqueness: { scope: :followed_user_id }
+    
+    belongs_to :user
+
+    has_many :followed_users,
+        foreign_key: :followed_user_id,
+        class_name: :user
 end
