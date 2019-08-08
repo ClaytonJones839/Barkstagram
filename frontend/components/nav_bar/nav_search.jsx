b2db2ac22a79a235c7030abdd053c47b573314de
Clayton <claytonajones839@gmail.com>
2019-08-08 12:10:01 -0400

    Add functioning search bar in nav bar

--- frontend/components/nav_bar/nav_search.jsx
+++ frontend/components/nav_bar/nav_search.jsx
@@ -6,22 +6,66 @@ class NavSearch extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            searchField: "",
+            // matchedUsers: ""
+            matchedUsers: []
+        }
     }
 
-
     componentDidMount() {
-        debugger
         this.props.fetchUsers();
     }
 
+    update(field) {
+        return e => {
+            let matches = this.props.users
+                .filter(user => user.username.toLowerCase()
+                    .includes(e.target.value.toLowerCase()));
+            if (!e.target.value) matches = [];
+            this.setState({
+                [field]: e.target.value,
+                matchedUsers: matches
+            });
+        };
+    }
+
+
     render() {
         
-    return(
-        <input
-            className="user-search"
-            type="text"
-            placeholder="Search Users"
-        />
+        let userMatches = this.state.matchedUsers.map(user => {
+            return(
+                <li 
+                    className="search-result-user"
+                    key={user.id}>
+                    <Link to={`/users/${user.id}`}>
+                        <img 
+                            className="search-result-image"
+                            src={user.photoUrl} />
+                        <span className="search-result-username">
+                            {user.username}
+                        </span>
+                    </Link>
+                </li>
+            )
+        })
+    
+    
+        return(
+        <div className="search-with-results-wrap">
+            <input
+                className="user-search"
+                type="text"
+                placeholder="Search Users"
+                onChange={this.update('searchField')}
+                value={this.state.searchField} 
+            />
+            <div className="search-results-container">
+                <ul className="search-results-list">
+                    {userMatches}
+                </ul>
+            </div>
+        </div>
     )}
 }
 
