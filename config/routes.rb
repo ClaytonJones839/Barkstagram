2bddc7d96ad908271e37f374dd8e94048306a647
Clayton <claytonajones839@gmail.com>
2019-08-08 11:16:38 -0400

    Add user#index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :show, :update] do 
+    resources :users, only: [:create, :show, :update, :index] do 
       resources :followings, only: [:index]
     end
     resource :session, only: [:create, :destroy, :show]
